300b6b86e86d2082e25e069221d349f09938a488
C0rTeZ13 <work.cortez13@gmail.com>
2025-04-28 17:53:35 +0300

    script fix

--- seog.sh
+++ seog.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+REAL_SCRIPT_PATH=$(readlink -f "${BASH_SOURCE[0]}")
+SCRIPT_DIR=$(dirname "$REAL_SCRIPT_PATH")
+
+cd "$SCRIPT_DIR" || exit 1
 
 if [[ -x "$SCRIPT_DIR/.venv/bin/seog" ]]; then
     "$SCRIPT_DIR/.venv/bin/seog" "$@"
-else
+elif [[ -f "$SCRIPT_DIR/pyproject.toml" ]]; then
     poetry run seog "$@"
+else
+    echo "Error: Neither virtualenv nor poetry project found!" >&2
+    exit 2
 fi
